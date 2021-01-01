0ec6e8bc912878098d98bbcdba8206678d627ade
mshafivk <mshafivk@gmail.com>
2021-01-01 20:40:07 +0530

    first commit

---
+++ arethmatic.py
@@ -0,0 +1,10 @@
+print(10+2)
+# Returns 3.3333...
+print(10/3)
+# returns 3
+print(10//3)
+
+# modulus
+print(10%3)
+# power
+print(10**3)
---
+++ read_input.py
@@ -0,0 +1,15 @@
+# name='John Smith'
+# age=26
+# is_new_patient=True
+# print('Name = ',name)
+# print("Age = ",age)
+# print("Is new patient = ",is_new_patient)
+# name=input("What is your name?")
+# print('Hi '+name)
+# ========================
+# name=input("What's your name? ")
+# color=input("What's your favourite color? ")
+# print(name+' likes '+color)
+# ========================
+birth_year=input('Enter your birth year ')
+print('Your age is =',2020-int(birth_year))
